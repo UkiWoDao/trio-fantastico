03e00dd7ab3f6270521d8aa7236201756fcc8cf2
Srdjan Rados <srdjan.rados90@gmail.com>
2022-05-13 16:24:47 +0200

    now whole pet object has updated values, new asserts for missing attributes

--- src/test/java/com/triofantastico/practiceproject/model/pet/Category.java
+++ src/test/java/com/triofantastico/practiceproject/model/pet/Category.java
@@ -1,13 +1,13 @@
 package com.triofantastico.practiceproject.model.pet;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     private Integer id;
