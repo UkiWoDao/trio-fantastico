8c8e6e3218d27210ef071fac6e8c3a01fd9422c9
Uros Vucenovic <uros.vucenovic@htecgroup.com>
2022-05-16 12:50:33 +0200

    add helper package with a util class for generating random numbers

---
+++ src/test/java/com/triofantastico/practiceproject/helper/RandomGenerator.java
@@ -0,0 +1,21 @@
+package com.triofantastico.practiceproject.helper;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class RandomGenerator {
+
+    private RandomGenerator() {
+        throw new AssertionError("Instantiation attempted from within class");
+    }
+
+    public static Integer getRandomNumber(int min, int max) {
+        ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+        return min + threadLocalRandom.nextInt(max);
+    }
+
+    public static Integer getRandomNumberPositiveNumberUpTo(int max) {
+        int min = 1;
+        ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+        return min + threadLocalRandom.nextInt(max);
+    }
+}
