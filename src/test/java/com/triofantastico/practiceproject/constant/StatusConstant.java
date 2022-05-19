9e2c28e556b8d6c7f82943ca289b0611336672c8
srdjanshelf <srdjan.rados@htecgroup.com>
2022-05-19 15:16:13 +0200

    fixed everything at once :D

--- src/test/java/com/triofantastico/practiceproject/constant/StatusConstant.java
+++ src/test/java/com/triofantastico/practiceproject/constant/StatusConstant.java
@@ -4,13 +4,10 @@ public interface StatusConstant {
 
     String STATUS_CODE_OK = "200";
     String STATUS_CODE = "1";
-
     String STATUS_CODE_NOT_FOUND = "404";
     String MESSAGE_NOT_FOUND = "Order not found";
-    String MESSAGE = "Orde321r not found";
-
+    String MESSAGE_TYPE = "unknown";
     String STATUS_CODE_TYPE = "error";
-
     String STATUS_CODE_MESSAGE = "Order Not Found";
 
 
