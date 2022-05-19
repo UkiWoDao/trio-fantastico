1165af0b86aed576d8d49f044fea61c7c4427608
Srdjan Rados <srdjan.rados90@gmail.com>
2022-05-19 10:02:03 +0200

    dsada

--- src/test/java/com/triofantastico/practiceproject/constant/StatusConstant.java
+++ src/test/java/com/triofantastico/practiceproject/constant/StatusConstant.java
@@ -7,6 +7,8 @@ public interface StatusConstant {
 
     String STATUS_CODE_NOT_FOUND = "404";
     String MESSAGE_NOT_FOUND = "Order not found";
+    String MESSAGE = "Orde321r not found";
+
     String STATUS_CODE_TYPE = "error";
 
     String STATUS_CODE_MESSAGE = "Order Not Found";
